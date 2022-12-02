f9c54c1d870dd4cef3f1d5ab8f5314b07f3e730b
Ian MacDougall <imacdoug@vols.utk.edu>
2022-12-02 13:04:47 -0500

    fixed halway typing

--- src/@types/TransactionHistory.d.ts
+++ src/@types/TransactionHistory.d.ts
@@ -1,3 +1,21 @@
+interface Order {
+  tx: string
+  serviceIndex: number
+  createdTimestamp: number
+  payer: {
+    id: string
+    name: string
+  }
+  consumer: {
+    id: string
+    name: string
+  }
+  amount: string
+  estimatedUSDValue: string
+  lastPriceToken: string
+  lastPriceValue: string
+}
+
 interface TransactionHistory {
   token: {
     id: string
@@ -6,25 +24,7 @@ interface TransactionHistory {
     publishMarketFeeAddress: string
     publishMarketFeeToken: string
     publishMarketFeeAmount: string
-    orders: [
-      {
-        tx: string
-        serviceIndex: number
-        createdTimestamp: number | Date
-        payer: {
-          id: string
-          name: string
-        }
-        consumer: {
-          id: string
-          name: string
-        }
-        amount: string
-        estimatedUSDValue: string
-        lastPriceToken: string
-        lastPriceValue: string
-      }
-    ]
+    orders: [Orders]
     dispensers: [
       {
         id: string
