036584e6e3a41f5f75d69b2c50faf27212b7e60f
Ian MacDougall <imacdoug@vols.utk.edu>
2023-03-31 14:35:59 -0400

    Synced ind. use. viz. and made Dr. Zacherie Sirett a default accessGranted

--- src/components/Asset/AssetContent/IndividualUseVisualization.tsx
+++ src/components/Asset/AssetContent/IndividualUseVisualization.tsx
@@ -7,18 +7,41 @@ import { useAsset } from '@context/Asset'
 import { AxisBottom, AxisLeft } from '@visx/axis'
 import { accountTruncate } from '@utils/web3'
 import Link from 'next/link'
+import mockedAccessEvents from 'content/static_data/mocked-access-events.json'
+
+const deniedEvents: Order[] = mockedAccessEvents.data.accessDenieds.map(
+  (obj) => ({
+    ...obj,
+    createdTimestamp: Number(obj.createdTimestamp),
+    amount: '1'
+  })
+)
+
+const grantedEvents: Order[] = mockedAccessEvents.data.accessGranteds.map(
+  (obj) => ({
+    ...obj,
+    createdTimestamp: Number(obj.createdTimestamp),
+    amount: '1'
+  })
+)
+
+const accessEvents: Order[] = [...deniedEvents, ...grantedEvents]
 
 const verticalMargin = 120
 const maxBarAmount = 10
 
-function payerIDMap(txs: TransactionHistory) {
+function payerIDMap(txs: Order[]) {
   const ids = new Map<string, number>()
-  const orders = txs?.token?.orders
+  const orders = txs
   for (let i = 0; i < orders?.length; i++) {
-    if (ids.has(orders[i].payer?.id)) {
-      ids.set(orders[i].payer?.id, ids.get(orders[i].payer?.id) + 1)
+    const idname = orders[i].payer?.name
+      ? orders[i].payer?.name
+      : orders[i].payer?.id
+
+    if (ids.has(idname)) {
+      ids.set(idname, ids.get(idname) + 1)
     } else {
-      ids.set(orders[i].payer?.id, 1)
+      ids.set(idname, 1)
     }
   }
   return new Map<string, number>([...ids.entries()].sort((a, b) => b[1] - a[1]))
@@ -52,13 +75,11 @@ export default function IndividualUseVisualization({
   height,
   events = false
 }: BarsProps) {
-  const { asset } = useAsset()
-  const txs: TransactionHistory = asset.transactionHistory
+  // const { asset } = useAsset()
+  // const txs: TransactionHistory = asset.transactionHistory
 
   // Parse the wallet addresses and store in a dictionary
-  const ids = payerIDMap(txs)
-  console.log(`ID Map Entries: ${[...ids.entries()]}`)
-  console.log(`ID Map Keys: ${[...ids.keys()][0]}`)
+  const ids = payerIDMap(accessEvents)
 
   // bounds
   const xMax = width - 30
@@ -92,34 +113,35 @@ export default function IndividualUseVisualization({
         />
         <Group top={verticalMargin / 2}>
           {[...ids.keys()].slice(0, maxBarAmount).map((d) => {
-            const address = d
+            const idname = d
             const barWidth = xScale.bandwidth()
-            const barHeight = yMax - (yScale(ids.get(address)) ?? 0)
-            const barX = xScale(address)
+            const barHeight = yMax - (yScale(ids.get(idname)) ?? 0)
+            const barX = xScale(idname)
             const barY = yMax - barHeight
             return (
-              <Group key={`bar-${address}`}>
+              <Group key={`bar-${idname}`}>
                 <Bar
-                  key={`bar-${address}`}
+                  key={`bar-${idname}`}
                   x={barX}
                   y={barY}
                   width={barWidth}
                   height={barHeight}
                   fill="#e44c4c"
                   onClick={() => {
-                    if (events) alert(`Wallet Address: ${address}`)
+                    if (events) alert(`Doctor's Name: ${idname}`)
                   }}
                 />
                 <text
-                  x={xScale(address) + barWidth / 2}
+                  x={xScale(idname) + barWidth / 2}
                   y={yMax - barHeight}
                   textAnchor="middle"
                   fill="#000000"
-                  fontSize={12}
+                  fontSize={15}
+                  fontWeight={'bold'}
                   dy={'-.33em'}
                   style={{ fontFamily: 'arial' }}
                 >
-                  {`${ids.get(address)}`}
+                  {`${ids.get(idname)}`}
                 </text>
               </Group>
             )
@@ -131,7 +153,7 @@ export default function IndividualUseVisualization({
             stroke="#000000"
             tickStroke="#000000"
             tickLabelProps={() => axisLeftTickLabelProps}
-            label="# of Transactions"
+            label="# of Accesses"
             labelProps={axisLabelProps} // Ocean Market removes textAnchor from props, but it still works
           />
           <AxisBottom
@@ -145,11 +167,11 @@ export default function IndividualUseVisualization({
                       x={tickRendererProps.x}
                       y={tickRendererProps.y}
                       textAnchor="middle"
-                      fontSize={10.5}
+                      fontSize={12}
                       fontWeight={'bold'}
                       fill={'black'}
                     >
-                      {accountTruncate(tickRendererProps.formattedValue)}
+                      {`Dr. ${tickRendererProps.formattedValue}`}
                     </text>
                   </a>
                 </Link>
